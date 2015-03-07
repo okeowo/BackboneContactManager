cf463aefcb571af4fb26bae4203aba8ae337f581
Ivan Manolov <ivan@MacBookPro.local>
2015-03-07 17:02:21 -0500

    removed popover, added jquery animation

--- BackboneContactManager/js/views/contactview.js
+++ BackboneContactManager/js/views/contactview.js
@@ -1,14 +1,19 @@
 var ContactView = Backbone.View.extend({
-	//get the undercore template
-	template: _.template($('#contactTemplate').html()),
+    //get the undercore template
+    template: _.template($('#contactTemplate').html()),
 
-	//always render on initialize so you don't have to later
-	initialize: function (){
-		this.render();
-	},
-	render: function (){
-		this.$el.html(this.template(this.model.toJSON()));
+    events: {
+        'click a': 'renderContactInfo',
+    },
 
+    //always render on initialize so you don't have to later
+    initialize: function() {
+        this.render();
+    },
+    render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+
+        /*
 		//set popover
 		var contactInfoTemplate = _.template($('#contactInfoTemplate').html());
 		var popoverTemplate = contactInfoTemplate(this.model.toJSON());
@@ -19,6 +24,22 @@ var ContactView = Backbone.View.extend({
 				return $.parseHTML(popoverTemplate);
 			}
 		});
-		return this;
-	}
+		*/
+        return this;
+    },
+
+    renderContactInfo: function() {
+        if (this.$el.find('a').hasClass('addHeight')) {
+            this.$el.find('a').removeClass('addHeight');
+            this.$el.find("div:nth-child(4)").toggle();
+            this.$el.find('a').animate({ height: '90px', 'margin-bottom':'50px' });
+
+        } 
+        else {
+            this.$el.find('a').animate({ height: '200px', 'margin-bottom': '0px'});
+            this.$el.find('a').addClass('addHeight');
+            this.$el.find("div:nth-child(4)").toggle();
+
+        }
+    }
 });
