f1dd5b86c17079eb28ef59c050ba8e4dd56c7a5c
Ivan Manolov <ivan@MacBookPro.local>
2015-03-07 17:22:32 -0500

    update css, fix up contact repo and animation overflow

--- BackboneContactManager/js/views/directoryview.js
+++ BackboneContactManager/js/views/directoryview.js
@@ -2,49 +2,49 @@
 // should be removed later
 var contacts = [{
     name: 'Contact 1',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'family'
 }, {
     name: 'Contact 2',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'family'
 }, {
     name: 'Contact 3',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'friend'
 }, {
     name: 'Contact 4',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'colleague'
 }, {
     name: 'Contact 5',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'family'
 }, {
     name: 'Contact 6',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'colleague'
 }, {
     name: 'Contact 7',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'friend'
 }, {
     name: 'Contact 8',
-    address: '1, a street, a town, a city, AB12 3CD',
+    address: '1 street, a town, a city, AB12 3CD',
     phone: '0123456789',
     email: 'anemail@me.com',
     group: 'family'
